0d76a09393f35f7ffd4110774497f1be9eacd119
David Lacho <david.lacho@gmail.com>
2018-12-18 20:21:11 -0800

    authorizes slackbot

--- routes/auth.js
+++ routes/auth.js
@@ -9,54 +9,14 @@ const request = require('request');
 // auth endpoints, do not prefix with '/auth'
 module.exports = (knex) => {
   passport.use(new SlackStrategy({
-    clientID: process.env.SLACK_CLIENT_ID,
-    clientSecret: process.env.SLACK_CLIENT_SECRET,
+    clientID: process.env.SLACK_BOT_CLIENT_ID,
+    clientSecret: process.env.SLACK_BOT_CLIENT_SECRET,
+    scope: ['bot'],
   }, (accessToken, refreshToken, profile, done) => {
     done(null, profile);
   }));
 
   router.get('/slack', passport.authorize('slack'));
-
-  // router.get('/slack/redirect',
-  //   passport.authorize('slack', {
-  //     failureRedirect: '/login',
-  //   }), (req, res) => {
-  //     const createJWT = (user) => {
-  //       const token = jwt.sign(user[0], process.env.PASSPORT_SECRET, {
-  //         expiresIn: Date.now() + 604800000, // 1 week
-  //       });
-  //
-  //       res.cookie('jwt', token);
-  //       res.redirect('/');
-  //     };
-  //
-  //     knex
-  //       .select('*')
-  //       .from('users')
-  //       .where('oauth_id', req.account.user.id)
-  //       .then((record) => {
-  //         if (record.length === 0) {
-  //           knex('users')
-  //             .insert({
-  //               name: req.account.user.name,
-  //               email: req.account.user.email,
-  //               oauth_id: req.account.user.id,
-  //             })
-  //             .catch((err) => {
-  //               console.log('there was an error: ', err);
-  //             })
-  //             .returning('*')
-  //             .then((newRecord) => {
-  //               createJWT(newRecord);
-  //             });
-  //         } else {
-  //           createJWT(record);
-  //         }
-  //       })
-  //       .catch((err) => {
-  //         console.log('there was an error: ', err);
-  //       });
-  //   });
 
   router.get('/slackbot/redirect', (req, res) => {
     const data = {
@@ -85,11 +45,11 @@ module.exports = (knex) => {
         bot_user_id: parsedBody.bot.bot_user_id,
         bot_access_token: parsedBody.bot.bot_access_token,
       };
-      console.log(insertObject);
+
       knex
         .select('*')
         .from('slack_bots')
-        .where('bot_user_id', parsedBody.bot.bot_user_id)
+        .where('bot_user_id', parsedBody.bot.bot_user_id || 0)
         .then((record) => {
           if (record.length === 0) {
             knex('slack_bots')
@@ -107,8 +67,7 @@ module.exports = (knex) => {
         })
         .catch((err) => {
           console.log('there was an error', err);
-        })
-
+        });
 
       if (!error && response.statusCode === 200) {
         res.redirect('/');
