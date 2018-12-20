1b2d27dc2eb88402f72e5c2d10f5488e44be27a7
David Lacho <david.lacho@gmail.com>
2018-12-19 20:15:52 -0800

    checking the data

--- routes/api.js
+++ routes/api.js
@@ -5,7 +5,7 @@ const passport = require('passport');
 const router = express.Router();
 
 // API endpoints, do not prefix with '/api'
-module.exports = () => {
+module.exports = (knex) => {
   router.get('/passwords', (req, res) => {
     const count = 5;
 
@@ -17,13 +17,28 @@ module.exports = () => {
   });
 
   router.post('/buildSurvey', passport.authenticate('jwt', {
-    session: false
+    session: false,
   }), (req, res) => {
-    console.log(req.user);
-    res.json('you made it.');
+
+    knex('users')
+      .join('slack_bots', 'slack_bots.creator_id', '=', 'users.slack_id')
+      .select('*')
+      .where('slack_id', req.user.creator_id)
+      .then((rows) => {
+        console.log(rows);
+        res.json(rows);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+
+    // req.user.id,
+    // req.user.access_token,
+    // req.user.creator_id,
+    // req.user.team_name,
+    // req.user.bot_user_id,
+    // req.user.bot_access_token
   });
-
-
 
   return router;
 };
