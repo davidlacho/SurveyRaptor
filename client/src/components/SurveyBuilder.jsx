86ec1fe665f602bb92744a5b3d5d151958fcec09
Jessica Winters <jskawinters@gmail.com>
2018-12-20 12:04:54 -0800

    Added survey builder form, added React Router

--- client/src/components/SurveyBuilder.jsx
+++ client/src/components/SurveyBuilder.jsx
@@ -3,28 +3,75 @@ import React, { Component } from 'react';
 class SurveyBuilder extends Component {
   constructor(props) {
     super(props);
-    this.state = {value: ''};
 
-    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      questions: [''],
+    }
+
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(event) {
-    this.setState({value: event.target.value});
+  handleText = i => e => {
+    let questions = [...this.state.questions];
+
+    questions[i] = e.target.value;
+
+    this.setState({
+      questions
+    });
+
+    console.log(questions);
+  }
+
+  handleDelete = index => event => {
+    event.preventDefault();
+
+    let questions = [
+      ...this.state.questions.slice(0, index),
+      ...this.state.questions.slice(index + 1)
+    ];
+
+    this.setState({
+      questions
+    });
+  }
+
+  addQuestion = e => {
+    e.preventDefault();
+
+    let questions = this.state.questions.concat(['']);
+
+    this.setState({
+      questions
+    });
   }
 
   handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value);
+    alert('A name was submitted: ' + this.state.questions);
     event.preventDefault();
   }
 
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
-        <label>
-          Your Question:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
-        </label>
+        {this.state.questions.map((question, index) => (
+          <div key={index}>
+            <input
+              type="text"
+              onChange={this.handleText(index)}
+              value={question}
+            />
+
+            <button onClick={this.handleDelete(index)}>
+              Delete
+            </button>
+          </div>
+        ))}
+
+        <button onClick={this.addQuestion}>
+          Add New Question
+        </button>
+
         <input type="submit" value="Submit" />
       </form>
     );
