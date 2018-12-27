dd43c37882040a80aaac042b05438d7f7db8735d
David Lacho <david.lacho@gmail.com>
2018-12-27 14:39:40 -0800

    logs out parsedBody to debug bot

--- routes/auth.js
+++ routes/auth.js
@@ -39,6 +39,7 @@ module.exports = (knex) => {
       };
 
       const parsedBody = JSON.parse(body);
+      console.log(parsedBody);
       if (parsedBody.ok) {
         const insertObject = {
           access_token: parsedBody.access_token,
