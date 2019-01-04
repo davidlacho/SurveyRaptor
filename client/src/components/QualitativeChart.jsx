17102e5f2f3e0ad127ed9a299cc57e5d3e6448b4
Jessica Winters <jskawinters@gmail.com>
2019-01-03 17:14:05 -0800

    Fixed code formatting

--- client/src/components/QualitativeChart.jsx
+++ client/src/components/QualitativeChart.jsx
@@ -1,16 +1,13 @@
-import React, {
-  Component
-} from 'react';
-
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 import axios from 'axios';
 import cookie from 'react-cookies';
+
+// Material-UI Components
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 import Avatar from '@material-ui/core/Avatar';
 import Chip from '@material-ui/core/Chip';
-import {
-  NavLink
-} from 'react-router-dom';
 
 class QualitativeChart extends Component {
   constructor(props) {
@@ -18,61 +15,65 @@ class QualitativeChart extends Component {
 
     this.state = {
       data: {},
-      team: {}
+      team: {},
     }
   }
 
   componentDidMount() {
     axios.get(`/api/team/`, {}, {
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${cookie.load('jwt')}`
-        }
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${cookie.load('jwt')}`
+      }
+    })
+    .then((results) => {
+      const teamObject = {}
+      results.data.forEach((res) => {
+        teamObject[res.id] = res;
       })
-      .then((results) => {
-        const teamObject = {}
-        results.data.forEach((res) => {
-          teamObject[res.id] = res;
-        })
-        this.setState({
-          team: teamObject,
-        })
+      this.setState({
+        team: teamObject,
       })
+    });
   }
 
   render() {
     const responseCards = (this.state.team ?
       this.state.data.responses.map((response, i) => {
         const tone = JSON.parse(response.tone).tones[0];
+
         return(
           <div key={response.id}>
             <h2>{response.question}</h2>
-            <Paper className='' elevation={1}>
-            <NavLink to={`/values/${response.slack_id}`}>
+
+            <Paper elevation={1}>
+              <NavLink to={`/values/${response.slack_id}`}>
+                <div>
+                  <Avatar alt={`${response.name}'s Avatar`} src={this.state.team[response.slack_id].profile.image_72} />
+                  <h3>{response.name}</h3>
+                </div>
+              </NavLink>
+
               <div>
-                <Avatar alt={`${response.name}'s Avatar`} src={this.state.team[response.slack_id].profile.image_72} className='' />
-                <h3>{response.name}</h3>
-              </div>
-            </NavLink>
-            <div>
-              <Typography variant="h5" component="h3">
-                {response.answer}
-              </Typography>
-              <Typography component="p">
-                <Chip label={`${tone.tone_name}: ${(tone.score * 100).toFixed(2)}%`} className='' variant="outlined" />
-              </Typography>
+                <Typography variant="h5" component="h3">
+                  {response.answer}
+                </Typography>
+                <Typography component="p">
+                  <Chip label={`${tone.tone_name}: ${(tone.score * 100).toFixed(2)}%`} variant="outlined" />
+                </Typography>
               </div>
             </Paper>
           </div>
         )
-        })
-      : '');
-
+      }
+    ): '');
 
     return (
-      <div> {responseCards} </div>
+      <React.Fragment>
+        {responseCards}
+      </React.Fragment>
     );
   }
-
 }
+
 export default QualitativeChart;
